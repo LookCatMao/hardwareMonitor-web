2b3d73a6c11d32ccb544225eadbbd4185b2d6500
luoyingbin <yellowrd07@gamil.com>
2021-12-31 15:01:22 +0800

    增加数据源模型

--- src/types/cpu.ts
+++ src/types/cpu.ts
@@ -18,18 +18,6 @@ export interface cpuType {
 			vendor: string
 			//型号
 			model: string
-			//总逻辑核心数
-			totalCores: number
-			//用户使用率
-			user: number
-			//系统使用率
-			sys: number
-			//当前等待率
-			wait: number
-			//错误率
-			nice: number
-			//总空闲率
-			idle: number
 			//总使用率
 			combined: number
 		}
