bf9b2d190682c747b20dfdef347fb3acb8cea47a
王常鑫 <curitistk@gmail.com>
2022-01-06 23:33:48 +0800

    1. 网络基础布局完成开发

--- src/views/network/index.vue
+++ src/views/network/index.vue
@@ -2,11 +2,26 @@
 	<div class="hw-network">
 		<div class="network-chart">
 			<div class="chart"></div>
-			<div class="legend"></div>
+			<div class="legend">
+				<p class="download">DOWNLOAD</p>
+				<p class="upload">UPLOAD</p>
+			</div>
 		</div>
 		<div class="network-usage">
-			<div class="download"></div>
-			<div class="upload"></div>
+			<div class="download usage-item">
+				<div class="title">DOWNLOAD</div>
+				<div class="speed">
+					<span class="value">9.24</span>
+					<span class="unit">Mbps</span>
+				</div>
+			</div>
+			<div class="upload usage-item">
+				<div class="title">UPLOAD</div>
+				<div class="speed">
+					<span class="value">12.34</span>
+					<span class="unit">Kbps</span>
+				</div>
+			</div>
 		</div>
 		<div class="type">NETWORK</div>
 	</div>
