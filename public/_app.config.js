0e505316836f3f13dcb134a0c0935d6ca990598f
王常鑫 <curitistk@gmail.com>
2022-01-06 10:18:06 +0800

    1. 调整外置配置文件内容

--- public/_app.config.js
+++ public/_app.config.js
@@ -1,11 +1,5 @@
-window.__CONF__ = {
-	// API_URL: "http://192.168.100.1:8090",
+window.__CONF__ = Object.freeze({
 	API_URL: "http://192.168.16.174:8090",
-	// API_URL: "",
-	API_URL_PREFIX: "/v1",
-	PH_LIMIT_MAX: 8.5,
-	PH_LIMIT_MIN: 6.5,
-	PH_COLOR_LIST: ["#e7676d", "#047ffa", "#462c83"]
-}
-Object.freeze(window.__CONF__)
+	API_URL_PREFIX: "/v1"
+})
 Object.defineProperty(window, "__CONF__", { configurable: false, writable: false })
